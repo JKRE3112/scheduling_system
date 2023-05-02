ec32d5234af18ea594daf6c877f4dbd4f40f0226
Arth <johncarandang571@gmail.com>
2023-05-02 22:02:22 +0800

    Delete fixed bug

--- delete.php
+++ delete.php
@@ -1,23 +1,24 @@
 <?php 
 
- $link = mysqli_connect("localhost", "root", "", "insertion");
- 
+$link = mysqli_connect("localhost", "root", "", "insertion");
+
 // Check connection
 if($link === false){
     die("ERROR: Could not connect. " . mysqli_connect_error());
 }
 
 $sql = "DELETE FROM faculty";
-$result = mysqli_query($link, $sql)
+$result = mysqli_query($link, $sql);
+
 if(isset($_GET['delete'])) { 
    
    if ($result) {
-   	    echo 'deleted!'
+   	    echo 'deleted!';
    } else {
    	echo "not deleted!";
    }
-} echo ".";
+}
 
-
+echo ".";
 
 ?>
